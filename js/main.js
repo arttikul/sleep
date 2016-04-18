9e46034a767eae510f0a30a718ad395f302e0234
Fedorenko Artyom <arttikul@gmail.com>
2016-04-18 15:54:23 +0300

    Added Header, Burger, main

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.hamburger').on('click', function(){
+  $('.menu').toggleClass('open');
+});
+
+$('.benefits-more').on('click', function(){
+  $('.benefits-block').toggleClass('benefits-open');
+});
