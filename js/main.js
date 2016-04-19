ca7e5866dc05e7be8ebbec1921637dcf0883f3d8
Fedorenko Artyom <arttikul@gmail.com>
2016-04-19 12:02:07 +0300

    Added animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
+// Гамбургер
 $('.hamburger').on('click', function(){
   $('.menu').toggleClass('open');
 });
-
+// закрытие гамбургера после клика по ссылке
+$('.menu>li>a').on('click', function(){
+  $('.menu').toggleClass('open');
+});
+// меню benefits
 $('.benefits-more').on('click', function(){
   $('.benefits-block').toggleClass('benefits-open');
+});
+// плавный скролл
+$(document).ready(function(){
+$('a.anchor').click(function (){
+var elementClick = $(this).attr("href");
+var destination = $(elementClick).offset().top;
+$('html,body').animate({
+scrollTop: destination
+}, 800);
+return false;
+});
 });
